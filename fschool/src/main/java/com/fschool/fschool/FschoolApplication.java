12433762af7419f0eb6109ef82e3d1b9a56c6c0d
seifgneedy <seifgneedy2@gmail.com>
2021-12-17 16:38:36 +0200

    Init

---
+++ fschool/src/main/java/com/fschool/fschool/FschoolApplication.java
@@ -0,0 +1,13 @@
+package com.fschool.fschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FschoolApplication.class, args);
+	}
+
+}
---
+++ fschool/src/test/java/com/fschool/fschool/FschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fschool.fschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
