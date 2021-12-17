cefeab0382e13ddf1963b40dfd964774658e305e
seifgneedy <seifgneedy2@gmail.com>
2021-12-17 21:37:21 +0200

    Annotated repos

--- fschool/src/main/java/com/fschool/fschool/Model/Course.java
+++ fschool/src/main/java/com/fschool/fschool/Model/Course.java
@@ -12,8 +12,8 @@ public class Course {
     private String code;
     @Column(nullable = false, unique = true)
     private String name;
-
-    @ManyToMany(cascade = {})       //todo
+    //TODO:
+    @ManyToMany(cascade = {})       
     @JoinTable(name = "member_of",
     joinColumns = @JoinColumn(name = "course_id"),
     inverseJoinColumns = @JoinColumn(name = "student_id"))
@@ -64,19 +64,19 @@ public class Course {
         members.add(user);
         user.getCourses().add(this);
     }
- 
+
     public void removeUser(User user) {
         members.remove(user);
         user.getCourses().remove(this);
     }
- 
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Course)) return false;
         return code != null && code.equals(((Course) o).getCode());
     }
- 
+
     @Override
     public int hashCode() {
         return getClass().hashCode();
--- fschool/src/main/java/com/fschool/fschool/Model/CourseRepository.java
+++ fschool/src/main/java/com/fschool/fschool/Model/CourseRepository.java
@@ -1,10 +1,9 @@
 package com.fschool.fschool.Model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface CourseRepository extends JpaRepository<Course, String> {
     Course findByCode(String Code);
     Course findByName(String Name);
-
-
 }
