1715c4449c2aaf79bb573d31e9323b692306f71a
seifgneedy <seifgneedy2@gmail.com>
2021-12-17 20:58:03 +0200

    Added Vuetify

--- fschool-frontend/src/router/index.js
+++ fschool-frontend/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import SignIn from "../views/SignIn.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "SignIn",
+    component: SignIn,
   },
   {
     path: "/about",
