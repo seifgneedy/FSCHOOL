be8e9a66ba06b3ef25f1895f7ea979b7e1759ebe
Ans-Elnagar <ansgom3a@gmail.com>
2021-12-18 17:46:15 +0200

    Add cookies

--- fschool/src/main/java/com/fschool/fschool/Controllers/SignInController.java
+++ fschool/src/main/java/com/fschool/fschool/Controllers/SignInController.java
@@ -1,7 +1,20 @@
 package com.fschool.fschool.Controllers;
+import javax.servlet.http.*;
 import org.springframework.web.bind.annotation.*;
 @RestController
 public class SignInController {
     
-    
+    @PostMapping("/sign-in")
+    public boolean signIn (@RequestParam String id,
+                        @RequestParam String password,
+                        HttpServletResponse response){
+        // Authinticate here
+        ////////////
+        ///////////
+        ////////////
+        Cookie cookie = new Cookie("id", id);
+        cookie.setMaxAge(8 * 24 * 60 * 60); // 8 days
+        response.addCookie(cookie);
+        return true;
+    }
 }
