4057dfce5a2f0cb47bccb6d240509647346e92dd
Ans-Elnagar <ansgom3a@gmail.com>
2021-12-21 01:28:44 +0200

    Admin drawer moved into AdminView Table of courses in Add/Edit course

---
+++ fschool-frontend/src/components/AddEditCourse.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <v-data-table
+      class="row-pointer"
+      :headers="headers"
+      :items="courses"
+      item-key="code"
+      @click:row="editCourse"
+    >
+    </v-data-table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      courses: [
+        {
+          code: "CS111",
+          name: "Introduction to CS",
+          members: [],
+        },
+        {
+          code: "CS112",
+          name: "Programming 1",
+          members: [],
+        },
+        {
+          code: "CS211",
+          name: "Data Structures",
+          members: [],
+        },
+        {
+          code: "CS223",
+          name: "Design Patterns",
+          members: [],
+        },
+        {
+          code: "CS311",
+          name: "Algorithms",
+          members: [],
+        },
+        {
+          code: "CS322",
+          name: "Networks",
+          members: [],
+        },
+      ],
+    };
+  },
+  computed: {
+    headers() {
+      return [
+        {
+          text: "Code",
+          align: "start",
+          value: "code",
+        },
+        {
+          text: "Name",
+          value: "name",
+        },
+      ];
+    },
+  },
+  methods: {
+    editCourse(row) {
+      console.log(row);
+    },
+  },
+};
+</script>
+<style lang="css" scoped>
+.row-pointer >>> tbody tr :hover {
+  cursor: pointer;
+}
+</style>
--- fschool-frontend/src/views/AdminView.vue
+++ fschool-frontend/src/views/AdminView.vue
@@ -1,20 +1,108 @@
 <template>
-    <v-app >
+  <v-app>
     <v-content>
-        <admin-nav  />
-        <div align="center">
-            <h1>Hello world</h1>
+      <div style="float: left">
+        <v-navigation-drawer permanent>
+          <v-list>
+            <v-list-item class="px-2"></v-list-item>
+              <v-list-item-avatar>
+                <v-img src="@/assets/admin_avatar.svg"></v-img>
+              </v-list-item-avatar>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title class="text-h6"> Admin </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <v-list nav dense>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-list-item-avatar>
+                  <v-img src="@/assets/add_course_icon.svg" />
+                </v-list-item-avatar>
+              </v-list-item-icon>
+              <v-list-item-title
+                @click="
+                  showAddEditCourse = true;
+                  showAddTeacher = false;
+                  showAddStudent = false;
+                "
+              >
+                Add / Edit Course
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-list-item-avatar>
+                  <v-img src="@/assets/teacher_avatar.svg" />
+                </v-list-item-avatar>
+              </v-list-item-icon>
+              <v-list-item-title
+                @click="
+                  showAddEditCourse = false;
+                  showAddTeacher = true;
+                  showAddStudent = false;
+                "
+              >
+                Add Teacher
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-list-item-avatar>
+                  <v-img src="@/assets/student_avatar.svg" />
+                </v-list-item-avatar>
+              </v-list-item-icon>
+              <v-list-item-title
+                @click="
+                  showAddEditCourse = false;
+                  showAddTeacher = false;
+                  showAddStudent = true;
+                "
+              >
+                Add Student
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </div>
+      <div align="center">
+        <div v-show="showAddEditCourse">
+          <add-edit-course />
+          <v-btn depressed color="primary" @click="addCourse">
+            Add Course
+          </v-btn>
         </div>
+        <div v-show="showAddTeacher"></div>
+        <div v-show="showAddStudent"></div>
+      </div>
     </v-content>
-    </v-app>
+  </v-app>
 </template>
 
 <script>
-import AdminNav from "@/components/AdminNav.vue";
+import AddEditCourse from "@/components/AddEditCourse.vue";
 export default {
-    name: "AdminView",
-    components:{
-        AdminNav,
-    }
-}
+  name: "AdminView",
+  components: {
+    AddEditCourse,
+  },
+  data() {
+    return {
+      showAddEditCourse: true,
+      showAddTeacher: false,
+      showAddStudent: false,
+      usersInCourse: [],
+    };
+  },
+  methods: {
+    addCourse() {
+      console.log("add course");
+    },
+  },
+};
 </script>
