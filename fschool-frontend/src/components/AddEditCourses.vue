e9cdfd0be40002d7fd14e86f0afa547b5606a9aa
Ans-Elnagar <ansgom3a@gmail.com>
2021-12-22 23:59:59 +0200

    Adding axios requests in components: AddEditCourses EditCourseMembers

--- fschool-frontend/src/components/AddEditCourses.vue
+++ fschool-frontend/src/components/AddEditCourses.vue
@@ -6,15 +6,9 @@
     class="elevation-1"
   >
     <template v-slot:top>
-      <v-toolbar
-        flat
-      >
+      <v-toolbar flat>
         <v-toolbar-title>Courses</v-toolbar-title>
-        <v-divider
-          class="mx-4"
-          inset
-          vertical
-        ></v-divider>
+        <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer></v-spacer>
         <v-dialog
           v-model="dialog"
@@ -23,67 +17,58 @@
           transition="dialog-bottom-transition"
         >
           <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              color="primary"
-              dark
-              class="mb-2"
-              v-bind="attrs"
-              v-on="on"
-            >
+            <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
               New Course
             </v-btn>
           </template>
           <v-card>
-            <v-toolbar
-          dark
-          color="primary"
-        >
-          <v-toolbar-title>{{ formTitle }}</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-toolbar-items>
-            <v-btn
-              v-show="isAddingNewCourse"
-              dark
-              text
-              @click="close"
-            >
-              Cancel
-            </v-btn>
-            <v-btn
-              dark
-              text
-              @click="save"
-            >
-              Save
-            </v-btn>
-          </v-toolbar-items>
-        </v-toolbar>
+            <v-toolbar dark color="primary">
+              <v-toolbar-title>{{ formTitle }}</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-toolbar-items>
+                <v-btn v-show="isAddingNewCourse" dark text @click="close">
+                  Cancel
+                </v-btn>
+                <v-btn dark text @click="save"> Save </v-btn>
+              </v-toolbar-items>
+            </v-toolbar>
             <v-card-text>
               <v-container>
                 <v-row v-if="isAddingNewCourse">
-                    <v-text-field 
-                      v-model="editedItem.code"
-                      label="Course code"
-                    ></v-text-field>
+                  <v-alert v-show="invalidCourseCode" type="error">
+                    Invalid course code
+                  </v-alert>
+                  <v-spacer v-show="invalidCourseCode"></v-spacer>
+                  <v-text-field
+                    v-model="editedItem.code"
+                    label="Course code"
+                    required
+                  ></v-text-field>
                 </v-row>
                 <v-row>
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="Course name"
-                    ></v-text-field>
+                  <v-text-field
+                    v-model="editedItem.name"
+                    label="Course name"
+                  ></v-text-field>
                 </v-row>
-                <v-row v-show="!isAddingNewCourse">
-                    <!--////////////////////
+                <v-row v-if="!isAddingNewCourse">
+                  <!--////////////////////
                     ///////////
                     /////////////////
                     / Start of Members table inside edit course
                     //////////////////
                     /
                     /////-->
-                    <edit-course-members :userRole="'teacher'" :courseCode="editedItem.code"></edit-course-members>
-                    <v-spacer></v-spacer>
-                    <edit-course-members :userRole="'student'" :courseCode="editedItem.code"></edit-course-members>
-                <!--////////////////////
+                  <edit-course-members
+                    :userRole="'teacher'"
+                    :courseCode="editedItem.code"
+                  ></edit-course-members>
+                  <v-spacer></v-spacer>
+                  <edit-course-members
+                    :userRole="'student'"
+                    :courseCode="editedItem.code"
+                  ></edit-course-members>
+                  <!--////////////////////
                     ///////////
                     /////////////////
                     / End of Members table inside edit course
@@ -97,11 +82,18 @@
         </v-dialog>
         <v-dialog v-model="dialogDelete" max-width="530px">
           <v-card align="center">
-            <v-card-title class="text-h5">Are you sure you want to delete {{editedItem.code}}?</v-card-title>
+            <v-card-title class="text-h5"
+              >Are you sure you want to delete
+              {{ editedItem.code }}?</v-card-title
+            >
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-btn color="blue darken-1" text @click="closeDelete"
+                >Cancel</v-btn
+              >
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                >OK</v-btn
+              >
               <v-spacer></v-spacer>
             </v-card-actions>
           </v-card>
@@ -109,200 +101,160 @@
       </v-toolbar>
     </template>
     <template v-slot:[`item.actions`]="{ item }">
-      <v-icon
-        small
-        class="mr-2"
-        @click="editItem(item)"
-      >
-        mdi-pencil
-      </v-icon>
-      <v-icon
-        small
-        @click="deleteItem(item)"
-      >
-        mdi-delete
-      </v-icon>
+      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
     </template>
   </v-data-table>
 </template>
 <script>
 import EditCourseMembers from "./EditCourseMembers.vue";
-  export default {
-    components: {
-    EditCourseMembers
+import { AXIOS } from "../http-common.js";
+export default {
+  components: {
+    EditCourseMembers,
   },
-    data: () => ({
-      dialog: false,
-      dialogDelete: false,
-      addMemberDialog: false,
-      dialogDeleteMember: false,
-      headers:[
-        {
-          text: "Code",
-          align: "start",
-          value: "code",
-        },
-        {
-          text: "Name",
-          value: "name",
-        },
-        { text: 'Actions', value: 'actions', sortable: false },
-      ],
-      userHeaders:[
-        {
-          text: "ID",
-          align: "start",
-          value: "id",
-        },
-        {
-          text: "Name",
-          value: "name",
-        },
-        {
-          text: "Role",
-          value: "role",
-        },
-        { text: 'Actions', value: 'actions', sortable: false },
-      ],
-      courses: [
-        {
-          code: "CS111",
-          name: "Introduction to CS",
-        },
-        {
-          code: "CS112",
-          name: "Programming 1",
-        },
-        {
-          code: "CS211",
-          name: "Data Structures",
-        },
-        {
-          code: "CS223",
-          name: "Design Patterns",
-        },
-        {
-          code: "CS311",
-          name: "Algorithms",
-        },
-        {
-          code: "CS322",
-          name: "Networks",
-        },
-      ],
-      editedIndex: -1,
-      deletedMemberIndex: -1,
-      isAddingNewCourse: true,
-      addedUserId: '',
-      editedItem: {
-        code: '',
-        name: '',
-        members:[],
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    addMemberDialog: false,
+    dialogDeleteMember: false,
+    invalidCourseCode: false,
+    headers: [
+      {
+        text: "Code",
+        align: "start",
+        value: "code",
       },
-      defaultItem: {
-        code: '',
-        name: '',
-        members:[],
+      {
+        text: "Name",
+        value: "name",
       },
-    }),
+      { text: "Actions", value: "actions", sortable: false },
+    ],
+    userHeaders: [
+      {
+        text: "ID",
+        align: "start",
+        value: "id",
+      },
+      {
+        text: "Name",
+        value: "name",
+      },
+      {
+        text: "Role",
+        value: "role",
+      },
+      { text: "Actions", value: "actions", sortable: false },
+    ],
+    courses: [],
+    editedIndex: -1,
+    deletedMemberIndex: -1,
+    isAddingNewCourse: true,
+    addedUserId: "",
+    editedItem: {
+      code: "",
+      name: "",
+    },
+    defaultItem: {
+      code: "",
+      name: "",
+    },
+  }),
 
-    computed: {
-      formTitle () {
-        return this.isAddingNewCourse ? 'New Course' : `Edit Course: ${this.editedItem.code}`;
-      },
+  computed: {
+    formTitle() {
+      return this.isAddingNewCourse
+        ? "New Course"
+        : `Edit Course: ${this.editedItem.code}`;
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    async initialize() {
+      // Getting current courses
+      await AXIOS.get("admin/courses", {}).then((res) => {
+        this.courses = res.data;
+      });
     },
 
-    watch: {
-      dialog (val) {
-        val || this.close()
-      },
-      dialogDelete (val) {
-        val || this.closeDelete()
-      },
+    editItem(item) {
+      this.editedIndex = this.courses.indexOf(item);
+      this.editedItem.code = item.code;
+      this.editedItem.name = item.name;
+      this.isAddingNewCourse = false;
+      this.dialog = true;
     },
 
-    created () {
-      this.initialize()
+    deleteItem(item) {
+      this.editedIndex = this.courses.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
     },
-
-    methods: {
-      initialize () {
-
-      },
-
-      editItem (item) {
-        this.editedIndex = this.courses.indexOf(item)
-        this.editedItem.code = item.code;
-        this.editedItem.name = item.name;
-        //Loading members here
-        /////////
-        this.editedItem.members = [];
-        // this.editedItem.members.push()
-        /////////
-        //////
-        this.isAddingNewCourse = false
-        this.dialog = true
-      },
-
-      deleteItem (item) {
-        this.editedIndex = this.courses.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialogDelete = true
-      },
-      deleteMember (item) {
-        this.deletedMemberIndex = this.editedItem.members.indexOf(item)
-        this.addedUserId = item.id;
-        this.dialogDeleteMember = true
-      },
-
-      deleteItemConfirm () {
-        this.courses.splice(this.editedIndex, 1)
-        this.closeDelete()
-      },
-      deleteMemberConfirm(){
-        this.editedItem.members.splice(this.deletedMemberIndex, 1);
-        this.closeDeleteMember();
-      },
-      close () {
-        this.dialog = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-          this.isAddingNewCourse = true
-        })
-      },
-      closeAddMemberDialog(){
-        this.addMemberDialog = false;
-        this.addedUserId = '';
-      },
-
-      closeDelete () {
-        this.dialogDelete = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-      closeDeleteMember(){
-        this.dialogDeleteMember = false;
-        this.addedUserId = '';
-      },
-      save () {
-        if (this.editedIndex > -1) {//Editing
-          Object.assign(this.courses[this.editedIndex], this.editedItem)
-        } else {// Adding new one
-          this.courses.push(this.editedItem)
+    async deleteItemConfirm() {
+      let response;
+      await AXIOS.delete(`admin/course?code=${this.editedItem.code}`, {}).then(
+        (res) => {
+          response = res.data;
         }
-        this.close()
-      },
-      saveAddedMember(){
-        // Send id
-        // this.addedUserId
-        // get user data 
-        // if success add it to the table
-        this.editedItem.members.push({id:'1512',name:'111', role:'Student'});
-        this.closeAddMemberDialog();
-      },
+      );
+      if (response) this.courses.splice(this.editedIndex, 1);
+      this.closeDelete();
     },
-  }
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+        this.isAddingNewCourse = true;
+        this.invalidCourseCode = false;
+      });
+    },
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+    async save() {
+      if (this.editedIndex > -1) {
+        //Editing
+        ///////////////
+        ////////////
+        //////////////////
+        //////////////
+        Object.assign(this.courses[this.editedIndex], this.editedItem);
+      } else {
+        // Adding new one
+        let response;
+        await AXIOS.post(
+          `admin/course?code=${this.editedItem.code}&name=${this.editedItem.name}`,
+          {}
+        ).then((res) => {
+          response = res.data;
+        });
+        if (response) this.courses.push(this.editedItem);
+        else {
+          this.invalidCourseCode = true;
+          return;
+        }
+      }
+      this.close();
+    },
+  },
+};
 </script>
