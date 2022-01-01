e03e1ef7b9a17085489b6e3193d61e0f0edd7a80
abdofawzy841 <58747792+abdofawzy841@users.noreply.github.com>
2022-01-01 14:08:07 +0200

    courseUI & posts component

---
+++ fschool-frontend/src/components/UserPosts.vue
@@ -0,0 +1,101 @@
+
+<template>
+<v-container fluid>
+  <v-col v-for="post in posts" :key="post.code">
+  <v-card
+    class="mx-auto"
+    color="#0F0639"
+    dark
+    max-width="600"
+  >
+    <v-card-title>
+      <v-icon
+        large
+        left
+      >
+      </v-icon>
+      
+      <span class="text-h6 font-weight-light">"Post Title"</span>
+    </v-card-title>
+
+    <v-card-text class="text-h5 font-weight-bold">
+      "Post body"
+    </v-card-text>
+
+    <v-card-actions>
+      <v-list-item class="grow" >
+        <v-list-item-avatar color="grey darken-3" >
+          <v-img
+            class="elevation-6"
+            alt=""
+            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+          ></v-img>
+        </v-list-item-avatar>
+
+        <v-card-actions>
+      <v-text
+        color="deep-purple lighten-2"
+        text
+      >
+        user@fschool.com
+      </v-text>
+    </v-card-actions>
+        
+
+        <v-row
+          align="center"
+          justify="end"
+        >
+        <span class="subheading mr-2">"time"</span>
+          <span class="mr-1">||</span>
+          <v-icon class="mr-1"  @click="MakeComment()
+          ">
+            mdi-comment-multiple-outline
+          </v-icon>
+         
+        </v-row>
+      </v-list-item>
+    </v-card-actions>
+   
+  </v-card>
+  </v-col>
+ </v-container>
+</template>
+<script>
+import { AXIOS } from "../http-common.js";
+export default {
+  props: ["PostType"],
+  components: {},
+  data: () => ({
+    posts: [],
+
+  }),
+
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+
+    async initialize() {
+      //TODO get this course's posts instead
+      // Getting  courses
+      await AXIOS.get("admin/courses", {})
+        .then((res) => {
+          this.posts = res.data;
+        })
+        .catch(() => {
+          this.networkError = true;
+        });
+      this.currentlyLoading = false;
+    },
+    goToCourse(code){
+      this.$router.push({path: `/${this.userRole}/${code}`})
+    },
+    MakeComment(){
+      console.log("sssssssssssss");
+    },
+  },
+};
+</script>
--- fschool-frontend/src/views/Course.vue
+++ fschool-frontend/src/views/Course.vue
@@ -1,14 +1,127 @@
+
 <template>
   <v-app>
     <v-main>
-      <div style="float: left">
+     <div style="float: left">
         <v-btn icon @click="goBack">
           <v-icon>mdi-keyboard-backspace</v-icon>
         </v-btn>
       </div>
+      <div style="float: left">
+        <v-navigation-drawer permanent>
+          <v-list>
+            <v-list-item class="px-2"></v-list-item>
+            <v-list-item-avatar>
+              <v-img src="@/assets/add_course_icon.svg"></v-img>
+            </v-list-item-avatar>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title 
+                    id="mainItem"
+                    style="font-size:20px"
+                > <h2 v-text="this.$route.params.code"></h2> </v-list-item-title> 
+                
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <v-list nav dense>
+            <v-list-item
+              link
+              @click="
+                showAssignments = true;
+                showPosts = false;
+                showAnnouncement = false;
+                showQuestions = false;
+              "
+            >
+              <v-list-item-icon>
+                <v-list-item-avatar>
+                  <v-img src="@/assets/assignments.png" />
+                </v-list-item-avatar>
+              </v-list-item-icon>
+             
+              <v-list-item-title
+                id="mainItem"
+              > Assignments </v-list-item-title>
+            </v-list-item>
+            <v-list-item
+              link
+              @click="
+                showAssignments = false;
+                showPosts = true;
+                showAnnouncement = false;
+                showQuestions=false;
+              "
+            >
+              <v-list-item-icon>
+                <v-list-item-avatar>
+                  <v-img src="@/assets/Posts.png" />
+                </v-list-item-avatar>
+              </v-list-item-icon>
+              <v-list-item-title
+                id="mainItem"
+              > posts </v-list-item-title>
+            </v-list-item>
+            <v-list-item
+              link
+              @click="
+                showAssignments = false;
+                showPosts = false;
+                showAnnouncement = true;
+                showQuestions= false;
+              "
+            >
+              <v-list-item-icon>
+                <v-list-item-avatar>
+                  <v-img src="@/assets/Announcement.png" />
+                </v-list-item-avatar>
+              </v-list-item-icon>
+              <v-list-item-title
+                id="mainItem"
+              > Annoncements </v-list-item-title>
+            </v-list-item>
+            <v-list-item
+              link
+              @click="
+                showAssignments = false;
+                showPosts = false;
+                showAnnouncement = false;
+                showQuestions = true;
+              "
+            >
+              <v-list-item-icon>
+                <v-list-item-avatar>
+                  <v-img src="@/assets/Qs.png"></v-img>
+                </v-list-item-avatar>
+              </v-list-item-icon>
+              
+              <v-list-item-title 
+                  id="mainItem"
+              > Questions </v-list-item-title>
+            </v-list-item> 
+            <br><br> <br><br><br>
+            <v-btn
+              block
+              dark
+              @click="signOut">Sign Out</v-btn>
+          </v-list>
+        </v-navigation-drawer>
+      </div>
       <div align="center">
-        <div>
-          <h2 v-text="this.$route.params.code"></h2>
+        <div v-show="showAssignments">
+          <user-posts  :PostType="'Assignment'" />
+        </div>
+        <div v-show="showPosts">
+          <user-posts  :PostType="'Posts'"/>
+        </div>
+        <div v-show="showAnnouncement">
+          <user-posts   :PostType="'Announcement'"/>
+        </div>
+        <div v-show="showQuestions">
+         <user-posts  :PostType="'Questions'"/>
         </div>
       </div>
     </v-main>
@@ -16,15 +129,25 @@
 </template>
 
 <script>
+import UserPosts from "../components/UserPosts.vue";
 export default {
   name: "course",
-  components: {},
+  components: {UserPosts,},
   data() {
     return {
       user: this.$store.getters.getUser,
+      showAssignments: true,
+      showPosts: false,
+      showAnnouncement: false,
+      showQuestions: false,
+      // usersInCourse: [],
     };
   },
   methods: {
+    signOut() {
+      this.$store.commit("signOut");
+      this.$router.push("/");
+    },
     goBack() {
       this.$router.go(-1);
     },
