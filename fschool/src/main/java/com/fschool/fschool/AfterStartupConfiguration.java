984cd9860cbb9a91d874850bb88da663f26e60a3
Ans-Elnagar <ansgom3a@gmail.com>
2022-01-04 02:16:29 +0200

    Startup configuration Adding the admin account if it doesn't already exist

---
+++ fschool/src/main/java/com/fschool/fschool/AfterStartupConfiguration.java
@@ -0,0 +1,31 @@
+package com.fschool.fschool;
+
+import java.time.LocalDate;
+
+import com.fschool.fschool.Model.Entities.User;
+import com.fschool.fschool.Model.Services.AdminService;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.*;
+
+@Component
+public class AfterStartUpConfiguration {
+    @Autowired
+	AdminService adminService;
+    @EventListener(ApplicationReadyEvent.class)
+    public void runAfterStartup() {
+        // Adding admin account if it doesn't exist
+		User admin = new User();
+		admin.setFirstName("Admin");
+		admin.setLastName("Account");
+		admin.setEmail("admin@admin.com");
+		admin.setPassword(DigestUtils.sha256Hex("turkish coffee"));
+		admin.setRole("admin");
+		admin.setSex("Male");
+        admin.setBirthDate(LocalDate.now());
+		adminService.addUser(admin);
+    }
+}
